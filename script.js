6968af5d3a885f63d5c820f727d1460017e311db
Matias <mgbrunini@gmail.com>
2022-04-26 23:40:55 -0300

    Dandole el toque!

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function clickHome(){
     document.getElementById("home_form").style.display="block";
-    document.getElementById("any_form").style.display="none";
+    document.getElementById("comestibles_form").style.display="none";
     document.getElementById("gaseosa_form").style.display="none";    
 }
 
-function clickAny(){
+function clickComestibles(){
     document.getElementById("home_form").style.display="none";
-    document.getElementById("any_form").style.display="block";
+    document.getElementById("comestibles_form").style.display="block";
     document.getElementById("gaseosa_form").style.display="none";
 }
 
-function resultadoAny(){
+function resultadoComestibles(){
     let costovar = parseInt(document.getElementById("costo").value);
     let ivavar = parseInt(document.getElementById("iva").value);
     let gananciavar = parseInt(document.getElementById("ganancia").value);
@@ -22,16 +22,16 @@ function resultadoAny(){
 
 function clickGaseosa(){
     document.getElementById("home_form").style.display="none";
-    document.getElementById("any_form").style.display="none";
+    document.getElementById("comestibles_form").style.display="none";
     document.getElementById("gaseosa_form").style.display="block";
 }
 
 function resultadoGaseosa(){
     let costovar = parseInt(document.getElementById("costoG").value);
     let impIntvar = parseInt(document.getElementById("impInt").value);
-    let ivavar = (parseInt(document.getElementById("iva").value))/100;
     let unidadesvar = parseInt(document.getElementById("unidades").value);
-    let result = (costovar - impIntvar)-ivavar;
+    let resta = (costovar - impIntvar);
+    let result = resta - (resta*0.21);
     let reResult = result / unidadesvar;
     let respuesta = "El precio final por unidad es $" + reResult.toFixed(2);
     document.getElementById("resultadoG").innerHTML = respuesta;
